85e34700df286ebc9d90a5915e3fe3489ea240c0
Tulip Pink <pinktulips@hotmail.com>
2019-01-09 11:52:30 -0800

    add

--- Challenges/shiftArray/shiftArray/Program.cs
+++ Challenges/shiftArray/shiftArray/Program.cs
@@ -6,13 +6,29 @@ namespace data_structures_and_algorithms
     {
         static void Main(string[] args)
         {
-            int[] inputArray = { 1, 5, 9, 11 };
+            int[] inputArray = new int[] { 1, 5, 9, 11 };
             int addedInt = 7;
+            int halfway = outputArray.Length / 2;
         }
-        static void int[] ArrayShift(int inputArray, int adddedInt);
+        static void ArrayShift(int inputArray, int adddedInt);
+
+        int[] outputArray = new int[outputArrayLength];
+
+        int outputArrayLength = new int { inputArray.Length + 1 };
         {
-        int outputArray = new int { inputArray.Length + 1 };
-    }
+         for (int i = 0; i < outputArray.Length; i++);
+        if (i == halfway)
+            {
+            outputArray[i] = addedInt
+            }
+        if else(i < halfway)
+                {
+            outputArray[i] = inputArray[i];
+        else(if i > halfway)
+               { outputArray[i] = inputArray[i -1]
+        }
+        
+
 
     }
 }
