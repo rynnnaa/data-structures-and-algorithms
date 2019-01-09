189c2281b9e96cc53ab148dd423339557e77e1e7
Tulip Pink <pinktulips@hotmail.com>
2019-01-09 10:49:40 -0800

    started challenge

---
+++ Challenges/shiftArray/shiftArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace data_structures_and_algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = { 1, 5, 9, 11 };
+            int addedInt = 7;
+        }
+        static void int[] ArrayShift(int inputArray, int adddedInt);
+        {
+        int outputArray = new int { inputArray.Length + 1 };
+    }
+
+    }
+}
