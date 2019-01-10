c36173de813ae586ead7c1a7617d0e74c050e36d
Tulip Pink <pinktulips@hotmail.com>
2019-01-10 10:14:45 -0800

    starting argument

---
+++ Challenges/array_binary_search/array_binary_search/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace array_binary_search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] binaryArray = new int[] { 1, 2, 3, 4, 5, 6 };
+            int searchKey = 3;
+            Console.WriteLine(BinarySearch(binaryArray, searchKey));
+        }
+
+        public static int BinarySearch (int[] binaryArray, int searchKey)
+        {
+            int min = 0;
+            int max = binaryArray.Length - 1;
+        }
+                
+        }
+    }
+}
