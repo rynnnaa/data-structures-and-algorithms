616e8ba21b1d30e96e641458e2f9409b1e2f5903
Tulip Pink <pinktulips@hotmail.com>
2019-01-10 10:52:18 -0800

    unit test set up

---
+++ Challenges/array_binary_search/binarySearchTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using array_binary_search;
+
+namespace binarySearchTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
