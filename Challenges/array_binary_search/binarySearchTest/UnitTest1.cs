54df61c6a5dcd51ca82df043c718b86559190f40
Tulip Pink <pinktulips@hotmail.com>
2019-01-10 11:01:02 -0800

    facts and theories

--- Challenges/array_binary_search/binarySearchTest/UnitTest1.cs
+++ Challenges/array_binary_search/binarySearchTest/UnitTest1.cs
@@ -13,5 +13,12 @@ namespace binarySearchTest
             int searchKey = 7;
             Assert.Equal(-1, Program.BinarySearch(binaryArray, searchKey));
         }
+        [Theory]
+        [InlineData(new int[] { 1, 3, 4, 5, 6, 7 }, 40, 3)]
+        [InlineData(new int[] { 1, 2, 4, 5, 6, 7 }, 40, 3)]
+        public void ReturnsIntTest(int[] binaryArray, int searchKey, int expectedIndex)
+        {
+            Assert.Equal(expectedIndex, Program.BinarySearch(binaryArray, searchKey));
+        }
     }
 }
