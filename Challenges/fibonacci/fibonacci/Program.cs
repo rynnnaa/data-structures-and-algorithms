a8a94dc47434d8006f95a04ae31f19cc2d0d3565
Tulip Pink <pinktulips@hotmail.com>
2019-01-13 20:48:53 -0800

    fibonacci

---
+++ Challenges/fibonacci/fibonacci/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = fibonacci(9);
+            Console.WriteLine(answer);
+        }
+
+        public static int fibonacci(int num)
+        {
+            int Num1 = 0;
+            int Num2 = 1;
+            int FibAnswer = 0;
+
+            for (int i = 2; i < num; i++)
+            {
+                FibAnswer = Num1 + Num2;
+                Num1 = Num2;
+                Num2 = FibAnswer;
+
+            }
+
+            return FibAnswer;
+        }
+
+    }
+}
---
+++ Challenges/matrix/matrix/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        int[,] array = new int[3, 3];
+
+        System.Console.WriteLine(array2D[0, 0]);
+
+System.Console.WriteLine(array2D[0, 1]);
+System.Console.WriteLine(array2D[1, 0]);
+System.Console.WriteLine(array2D[1, 1]);
+System.Console.WriteLine(array2D[3, 0]);
+System.Console.WriteLine(array2Db[1, 0]);
+System.Console.WriteLine(array3Da[1, 0, 1]);
+System.Console.WriteLine(array3D[1, 1, 2]);
+    }
+}
