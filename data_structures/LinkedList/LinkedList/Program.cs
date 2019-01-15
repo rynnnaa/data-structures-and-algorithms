87a488804a07f9a66f29f0133c8cb2aad9638594
Tulip Pink <pinktulips@hotmail.com>
2019-01-15 11:25:19 -0800

    Make node list

--- data_structures/LinkedList/LinkedList/Program.cs
+++ data_structures/LinkedList/LinkedList/Program.cs
@@ -22,7 +22,28 @@ namespace LinkedList
 
         }
 
+        static void NodeList()
+        {
+            LList list = new LList();
+            list.Insert(2);
+            list.Insert(4);
+            list.Insert(5);
+            list.Insert(6);
+            list.Insert(8);
+            list.Insert(9);
+            list.Insert(1);
 
+            list.Append(15);
+            list.Append(16);
+            list.Append(17);
+
+            list.InsertBefore(8, 7);
+
+            list.InsertAfter(9, 10);
+
+            list.Nodes();
+            Console.ReadLine();
+        }
     }
 }
 
