53ff20e59f7208195ea2326c2e4325068b43d455
Tulip Pink <pinktulips@hotmail.com>
2019-01-16 10:30:08 -0800

    logic

--- Challenges/LinkedList/LinkedList/Program.cs
+++ Challenges/LinkedList/LinkedList/Program.cs
@@ -7,18 +7,54 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ValueAtEnd(4);
         }
 
         public static int ValueAtEnd(int value)
         {
-            int num = 0;
+            int runner = 0;
             int coutner = 0;
 
-            LinkedList linkedlist = new LinkedList();
+            LList list = new LList();
 
 
-                list.insert(2)
-                list.insert(3)
+            list.insert(2);
+            list.insert(3);
+
+            list.Current = list.Head;
+
+            while (list.Current.Next != null)
+            {
+                counter++;
+                if (counter >= value)
+                {
+                    runner++;
+                }
+
+                int counter2 = 0;
+
+                list.Current = list.Head;
+
+                while (list.Current.Next != null)
+                {
+                    counter2++;
+
+                    if (runner == counter2)
+                    {
+                        Console.WriteLine(list.Current.Value);
+
+                        return list.Current.Value;
+                    }
+
+                    list.Current = list.Current.Next;
+                    {
+
+                        return -1;
+                    }
+
+
+                }
+            }
         }
     }
 }
