a5ee53f88cba9ef3aaa7de2890d71379f156a36b
Tulip Pink <pinktulips@hotmail.com>
2019-01-16 10:22:43 -0800

    decalring counter and creating insert list

---
+++ Challenges/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int ValueAtEnd(int value)
+        {
+            int num = 0;
+            int coutner = 0;
+
+            LinkedList linkedlist = new LinkedList();
+
+
+                list.insert(2)
+                list.insert(3)
+        }
+    }
+}
