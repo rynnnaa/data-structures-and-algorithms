1b89fcc4ae916ee054be68ab0f029bb4cdac35b9
Tulip Pink <pinktulips@hotmail.com>
2019-01-17 10:46:52 -0800

    make two linked lists

--- Challenges/mergeLinkList/mergeLinkList/Program.cs
+++ Challenges/mergeLinkList/mergeLinkList/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using LinkedList.Class;
+using System;
 
 namespace mergeLinkList
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            LList list = new LList();
+            list.Insert(1);
+            list.Insert(3);
+            list.Insert(5);
+            list.Insert(7);
+
+            LList listTwo = new LList();
+            list.Insert(2);
+            list.Insert(4);
+            list.Insert(6);
+            list.Insert(8);
+
+        }
+
+        public static void LList Merge(LList list, LList listTwo)
+        {
+
         }
     }
 }
