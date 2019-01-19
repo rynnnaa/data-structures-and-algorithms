6b302a560dc4256870f355ca3c9c0f0420815137
Tulip Pink <pinktulips@hotmail.com>
2019-01-19 12:38:03 -0800

    start unit testing

---
+++ data_structures/LinkedList/LinkedList TEST/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using LinkedList;
+
+namespace LinkedList_TEST
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
