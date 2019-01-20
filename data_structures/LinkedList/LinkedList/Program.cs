a9a5b27159b48edde819b33d0ebd6b864a88be57
Tulip Pink <pinktulips@hotmail.com>
2019-01-19 23:02:06 -0800

    update

--- data_structures/LinkedList/LinkedList/Program.cs
+++ data_structures/LinkedList/LinkedList/Program.cs
@@ -5,22 +5,31 @@ namespace LinkedList
 {
     public class Program
     {
+//call method
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            CreateNodeAndList();
 
-
-            LList list = new LList();
-            int[] arr = { 1, 2, 3, 4, 5, 6 };
-            for (int i = 0; i < arr.Length; i++)
-            {
-                list.Insert(arr[i]);
-
-            }
-
-            list.Print();
         }
 
-        
+        /// <summary>
+        /// Creates a new Linked list and inserts new Nodes
+        /// </summary>
+        static void CreateNodeAndList()
+        {
+            LList list = new LList();
+            list.Insert(1);
+            list.Insert(2);
+            list.Insert(3);
+            list.Insert(4);
+            list.Insert(5);
+            list.Insert(6);
+
+            Console.WriteLine($"Does 2 exist: {list.Includes(3)}");
+            Console.WriteLine($"Does 5 exist: {list.Includes(5)}");
+            list.PrintNodes();
+            Console.ReadLine();
+        }
     }
 }
