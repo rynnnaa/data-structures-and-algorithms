bb144f79514a759b1aca2bea707b2379b0ff7df3
Tulip Pink <pinktulips@hotmail.com>
2019-01-20 18:52:01 -0800

    added tests

--- data_structures/LinkedList/linkListTest/UnitTest1.cs
+++ data_structures/LinkedList/linkListTest/UnitTest1.cs
@@ -50,5 +50,49 @@ namespace linkListTest
             Assert.True(found);
         }
 
+
+        [Fact]
+        public void AddNumber()
+        {
+            //arrange
+            LList list = new LList();
+            list.Insert(2);
+
+            //act
+            bool found = list.Includes(2);
+
+            //assert
+            Assert.True(found);
+        }
+
+        //testing insert method
+
+        [Fact]
+        public void NodeValueIsTwo()
+        {
+            LList testList = new LList();
+            testList.Insert(2);
+            Assert.True(testList.Head.Value == 2);
+        }
+
+        [Fact]
+        public void LastInsertIsHead()
+        {
+            LList testList = new LList();
+            testList.Insert(4);
+            testList.Insert(2);
+            bool isHead = testList.Head.Value == 2;
+            Assert.True(isHead);
+        }
+
+        [Fact]
+        public void NodeHasNoCurrent()
+        {
+            LList testList = new LList();
+            testList.Insert(1);
+            bool current = testList.Current == null;
+            Assert.True(current);
+        }
+
     }
 }
