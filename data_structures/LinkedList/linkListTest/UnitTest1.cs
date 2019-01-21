fb12b1d6df0c6d4055dc2a11f371d16f8e9873a5
Tulip Pink <pinktulips@hotmail.com>
2019-01-20 16:33:06 -0800

    add to unit testing

--- data_structures/LinkedList/linkListTest/UnitTest1.cs
+++ data_structures/LinkedList/linkListTest/UnitTest1.cs
@@ -6,48 +6,66 @@ namespace linkListTest
 {
     public class UnitTest1
     {
+        [Fact]
+        public void Find()
+        {
+            //arrange
+            LList list = new LList();
+            list.Insert(2);
 
-            [Fact]
-            public void Find()
-            {
-                //arrange
-                LList list = new LList();
-                list.Insert(2);
+            //act
+            bool found = list.Includes(2);
 
-                //act
-                bool found = list.Includes(2);
-
-                //assert
-                Assert.True(found);
-            }
-
-            [Fact]
-            public void CanNotFound()
-            {
-                //arrange
-                LList list = new LList();
-                list.Insert(2);
-
-                //act
-                bool found = list.Includes(9);
-
-                //assert
-                Assert.False(found);
-            }
-
-            [Fact]
-            public void AddNumber()
-            {
-                //arrange
-                LList list = new LList();
-                list.Insert(2);
-
-                //act
-                bool found = list.Includes(2);
-
-                //assert
-                Assert.True(found);
-            }
-
+            //assert
+            Assert.True(found);
         }
+
+        [Fact]
+
+        public void CanFind2()
+        {
+            //arrange
+            LList list = new LList();
+            list.Insert(2);
+
+            //act
+            bool found = list.Includes(2);
+
+            //assert
+            Assert.True(found);
+        }
+
+        [Fact]
+        public void CanFind3()
+        {
+            //arrange
+            LList list = new LList();
+            list.Insert(3);
+
+            //act
+            bool found = list.Includes(3);
+
+            //assert
+            Assert.True(found);
+        }
+
+
+        [Fact]
+        public void AddNumber()
+        {
+            //arrange
+            LList list = new LList();
+            list.Insert(2);
+
+            //act
+            bool found = list.Includes(2);
+
+            //assert
+            Assert.True(found);
+        }
+
+
+
+
+    }
     }
