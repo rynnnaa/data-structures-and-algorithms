1a409af581febcde4876aae38157f753c8f7ff2a
Tulip Pink <pinktulips@hotmail.com>
2019-01-22 09:41:06 -0800

    setting up classes

---
+++ data_structures/StacksAndQueues/StacksAndQueues/Classes/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues.Classes
+{
+    class Node
+    {
+
+        public int Value{ get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
