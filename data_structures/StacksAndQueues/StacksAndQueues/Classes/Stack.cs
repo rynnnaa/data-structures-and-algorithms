79381c7172e2d76e2c261320061b4a2292284aa1
Tulip Pink <pinktulips@hotmail.com>
2019-01-22 09:47:57 -0800

    push

--- data_structures/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
+++ data_structures/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
@@ -6,5 +6,21 @@ namespace StacksAndQueues.Classes
 {
     class Stack
     {
+        public Node Top { get; set; }
+
+        public Stack(Node node)
+        {
+            Top = node;
+        }
+
+        public Stack()
+        {
+
+        }
+
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+        }
     }
 }
