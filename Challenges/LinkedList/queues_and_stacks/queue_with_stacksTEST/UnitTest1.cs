06260e03bcbb0a5dec58d241610d87715dbf02a8
Tulip Pink <pinktulips@hotmail.com>
2019-01-23 12:42:14 -0800

    tests work

--- Challenges/LinkedList/queues_and_stacks/queue_with_stacksTEST/UnitTest1.cs
+++ Challenges/LinkedList/queues_and_stacks/queue_with_stacksTEST/UnitTest1.cs
@@ -62,6 +62,61 @@ namespace queue_with_stacksTEST
             Equals(stack.Top.Value);
         }
 
+        [Fact]
+        public void DequeueWorks()
+        {
+            //arrange
+            Stack stack = new Stack();
+            Stack stack2 = new Stack();
+
+            PseudoQueue queue = new PseudoQueue();
+            queue.Enqueue(stack, 1);
+            queue.Enqueue(stack, 2);
+            queue.Enqueue(stack, 3);
+
+            //act
+            queue.Dequeue(stack, stack2);
+
+            //assert
+            Assert.Equal(2, stack2.Top.Value);
+        }
+
+
+        [Fact]
+        public void DequeueWorks2()
+        {
+            //arrange
+            Stack stack = new Stack();
+            Stack stack2 = new Stack();
+            PseudoQueue queue = new PseudoQueue();
+            queue.Enqueue(stack, 2);
+            queue.Enqueue(stack, 3);
+            queue.Enqueue(stack, 4);
+
+            //act
+            queue.Dequeue(stack, stack2);
+
+            //assert
+            Assert.Equal(3, stack2.Top.Value);
+        }
+
+        [Fact]
+        public void DequeueWorks3()
+        {
+            //arrange
+            Stack stack = new Stack();
+            Stack stack2 = new Stack();
+            PseudoQueue queue = new PseudoQueue();
+            queue.Enqueue(stack, 3);
+            queue.Enqueue(stack, 4);
+            queue.Enqueue(stack, 5);
+
+            //act
+            queue.Dequeue(stack, stack2);
+
+            //assert
+            Assert.Equal(4, stack2.Top.Value);
+        }
 
     }
 }
