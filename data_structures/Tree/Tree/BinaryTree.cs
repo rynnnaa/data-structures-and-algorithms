5fee0e7459de0d8ad00dfc49490c443a13f4f635
Tulip Pink <pinktulips@hotmail.com>
2019-01-28 11:43:51 -0800

    created classes

---
+++ data_structures/Tree/Tree/BinaryTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        public List<int> ArrList = new List<int>();
+
+        public BinaryTree(Node node)
+        {
+            Root = node;
+        }
+    }
