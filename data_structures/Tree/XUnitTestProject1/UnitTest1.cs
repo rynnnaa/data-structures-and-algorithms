f0191a9c67374e2b5e8170bf21fc453962f66935
Tulip Pink <pinktulips@hotmail.com>
2019-01-28 13:30:20 -0800

    preorder works

--- data_structures/Tree/XUnitTestProject1/UnitTest1.cs
+++ data_structures/Tree/XUnitTestProject1/UnitTest1.cs
@@ -25,5 +25,42 @@ namespace XUnitTestProject1
             Assert.Equal(1, result[0]);
 
         }
+
+        [Fact]
+        public void PreOrderWork2()
+        {
+            //arrange
+            Node node = new Node(1);
+            BinaryTree tree = new BinaryTree(node);
+            node.Left = new Node(2);
+            node.Left.Left = new Node(3);
+            node.Left.Right = new Node(4);
+            node.Right = new Node(5);
+
+            //act
+            int[] result = tree.PreOrder(node);
+
+            //assert
+            Assert.Equal(3, result[2]);
+        }
+
+        [Fact]
+        public void PreOrderWorksAgain3()
+        {
+            //arrange
+            Node node = new Node(1);
+            BinaryTree tree = new BinaryTree(node);
+            node.Left = new Node(2);
+            node.Left.Left = new Node(3);
+            node.Left.Right = new Node(4);
+            node.Right = new Node(5);
+
+            //act
+            int[] result = tree.PreOrder(node);
+
+            //assert
+            Assert.Equal(5, result[4]);
+
+        }
     }
 }
