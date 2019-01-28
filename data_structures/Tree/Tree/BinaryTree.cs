7570f9a3bd11e1d650520fe52697aff2a43976e3
Tulip Pink <pinktulips@hotmail.com>
2019-01-28 12:16:53 -0800

    binary tree

--- data_structures/Tree/Tree/BinaryTree.cs
+++ data_structures/Tree/Tree/BinaryTree.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Tree
+namespace Tree.Classes
 {
     public class BinaryTree
     {
@@ -14,4 +14,73 @@ namespace Tree
         {
             Root = node;
         }
+
+        /// <summary>
+        /// Starts with the root, will return root first before going to  it's children
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns>An array</returns>
+        /// 
+        public int[] PreOrder(Node root)
+        {
+            ArrList.Add(root.Value);
+
+            if (root.Left != null)
+            {
+                PreOrder(root.Left);
+            }
+
+            if (root.Right != null)
+            {
+                PreOrder(root.Right);
+            }
+
+            return ArrList.ToArray();
+        }
+
+        /// <summary>
+        /// The will return nodes in order from left to right, returns root in between
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns>An array</returns>
+        public int[] InOrder(Node root)
+        {
+            if (root.Left != null)
+            {
+                InOrder(root.Left);
+            }
+
+            ArrList.Add(root.Value);
+
+            if (root.Right != null)
+            {
+                InOrder(root.Right);
+            }
+
+            return ArrList.ToArray();
+        }
+
+        /// <summary>
+        /// Returns Nodes left, then right then Root
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns>An array</returns>
+        /// 
+        public int[] PostOrder(Node root)
+        {
+            if (root.Left != null)
+            {
+                PostOrder(root.Left);
+            }
+
+            if (root.Right != null)
+            {
+                PostOrder(root.Right);
+            }
+
+            ArrList.Add(root.Value);
+
+            return ArrList.ToArray();
+        }
     }
+}
