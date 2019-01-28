438b44f9fe92fcae88349e7fbff1f10408f07a7c
Tulip Pink <pinktulips@hotmail.com>
2019-01-28 12:21:35 -0800

    add node

--- data_structures/Tree/Tree/BinarySearchTree.cs
+++ data_structures/Tree/Tree/BinarySearchTree.cs
@@ -21,5 +21,54 @@ namespace Tree.Classes
                 Add(Root, node.Value);
             }
         }
+
+        /// <summary>
+        /// Using greater than or less than logic to determine where to add the node
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="value"></param>
+        /// <returns>node</returns>
+        /// 
+        public Node Add(Node root, int value)
+        {
+
+            Node node = new Node(value);
+
+            if (root == null)
+            {
+                root = node;
+                return node;
+            }
+
+            Node parent = null;
+
+            while (root != null)
+            {
+                parent = root;
+
+                if (value < root.Value)
+                {
+                    root = root.Left;
+                }
+
+                else
+                {
+                    root = root.Right;
+                }
+            }
+
+            if (value <= parent.Value)
+            {
+                parent.Left = node;
+            }
+            else
+            {
+                parent.Right = node;
+            }
+
+            return node;
+        }
+
+     
     }
 }
