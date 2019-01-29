932ebed91bb6adbbbfefc68e5397abd8241f6429
Tulip Pink <pinktulips@hotmail.com>
2019-01-29 10:31:23 -0800

    check nodes modulo 3 and 5

---
+++ Challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Tree;
+using Tree.Classes;
+
+namespace FizzBuzzTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+        /// <summary>
+        /// Checks each node for divisibility by 3, or 5, and
+        /// changes value to 'FizzBuzz', 'Fizz', or 'Buzz' (respectively) if yes
+        /// </summary>
+        /// <param name="node"> node to check </param>
+        public static object[] FizzBuzz(Node root)
+        {
+            List<object> arrList = new List<object>();
+
+            if (root.Value % 15 == 0)
+            {
+                root.Value = "FizzBuzz";
+            }
+            else if ((int)root.Value % 3 == 0)
+            {
+                root.Value = "Fizz";
+            }
+            else if ((int)root.Value % 5 == 0)
+            {
+                root.Value = "Buzz";
+            }
+
+            arrList.Add(root.Value);
+
+            if (root.Left != null)
+            {
+                FizzBuzz(root.Left);
+            }
+            if (root.Right != null)
+            {
+                FizzBuzz(root.Right);
+            }
+
+            return arrList.ToArray();
+        }
+    }
+    }
+}
