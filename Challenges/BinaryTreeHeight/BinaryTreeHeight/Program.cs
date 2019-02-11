68747241576367545b897319e4b27cc6324d8617
Tulip Pink <pinktulips@hotmail.com>
2019-02-11 15:46:01 -0800

    initial

---
+++ Challenges/BinaryTreeHeight/BinaryTreeHeight/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Tree.Classes;
+
+namespace BinaryTreeHeight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+       public static int BinaryTreeHeight(Node root)
+        {
+
+        }
+    }
+}
