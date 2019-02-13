1048f02e5338bd35d9d7847aded73c563879007e
Tulip Pink <pinktulips@hotmail.com>
2019-02-13 11:05:38 -0800

    method

---
+++ Challenges/insertion-sort/insertion-sort/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace insertion_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void InsertionSort()
+        {
+
+        }
+    }
+}
