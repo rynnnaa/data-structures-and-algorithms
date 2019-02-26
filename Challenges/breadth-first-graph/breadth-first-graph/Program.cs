76d2fa664b0c9b59dc5d2952d6a68b7ef4a3a53c
Tulip Pink <pinktulips@hotmail.com>
2019-02-26 09:53:10 -0800

    breadth_first

---
+++ Challenges/breadth-first-graph/breadth-first-graph/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace breadth_first_graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public List<Node> BreadthFirst(Node root)
+        {
+            List<Node> order = new List<Node>();
+            Queue<Node> breadth = new Queue<Node>();
+            breadth.Enqueue(root);
+
+            while (breadth.TryPeek(out root))
+            {
+                Node front = breadth.Dequeue();
+                order.Add(front);
+
+                foreach (Node child in front.Children)
+                {
+                    if (!child.Visited)
+                    {
+                        child.Visited = true;
+                        breadth.Enqueue(child);
+                    }
+                }
+            }
+
+            return order;
+        }
+    }
+}
